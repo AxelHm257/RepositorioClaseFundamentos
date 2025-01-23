d5703743791d19478bf4311d0f1fb54e38b33fbe
AxelHm257 <123045383@upq.edu.mx>
2025-01-23 15:56:58 -0600

    Create Codigo estructuras.java
    Se trabajo con la sintaxis

---
+++ Codigo estructuras.java
@@ -0,0 +1,36 @@
+import.java.util.Scanner;
+
+public class Main{
+	public static void public static void main(String[] args) {
+	
+		String cadena = "333", cadena2, cadena3;
+		int entero = 3, entero2, entero3, numd;
+		double decimal = 3.3, decimal2, decimal3;
+
+		// Convertir cadena a entero 
+		entero2 = Integer.parseInt(cadena);
+		System.out.println(entero2);
+
+		// Convertir double a flotante 
+		entero3 = (int) decimal;
+		System.out.println(entero3);
+
+		//Convertir double a cadena
+		cadena2 = String.valueOf(decimal);
+
+		// Convertir int a double 
+		decimal2 = Double.valueOf(entero);
+
+		//Conversion Implícita 
+		int i = 12;
+		double numd = 1;
+		System.out.println(numd);
+
+
+		// 4. Solicitud de datos 
+		
+
+
+	}
+
+}
